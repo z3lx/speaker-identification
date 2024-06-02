d4e85cebcbe2f9b616175447ca1893162f1029f8
z3lx <57017122+z3lx@users.noreply.github.com>
2024-06-02 16:06:09 -0400

    Improve embeddings caching

--- identify.py
+++ identify.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import logging
 import os
@@ -15,9 +16,27 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
+def hash_file(
+    file_path: str
+) -> bytes:
+    """
+    Compute the SHA-256 hash of a file.
+
+    Parameters:
+        file_path (str): Path to the file.
+
+    Returns:
+        bytes: SHA-256 hash of the file.
+    """
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.digest()
+
+
 def compute_embeddings(
-    directory: str,
-    embeddings: Dict[str, np.ndarray],
+    audio_index: Dict[str, Tuple[np.ndarray, bytes]],
     inference: Inference,
     show_tqdm: bool = True,
     tqdm_kwargs: Dict[str, Any] = None
@@ -26,9 +45,7 @@ def compute_embeddings(
     Compute embeddings for audio files in a given directory.
 
     Parameters:
-        directory (str): Path to the directory containing the audio files.
-        embeddings (Dict[str, np.ndarray]): Dictionary to modify in-place with computed embeddings.
-            Keys are file paths, values are embeddings.
+        audio_index (Dict[str, Tuple[np.ndarray, bytes]]): Dictionary to modify in-place with computed embeddings.
         inference (Inference): Inference object used to compute embeddings.
         show_tqdm (bool, optional): Whether to show a tqdm progress bar.
         tqdm_kwargs (Dict[str, Any], optional): Additional arguments for the tqdm progress bar.
@@ -36,35 +53,28 @@ def compute_embeddings(
     Returns:
         None: The embeddings are modified in-place.
     """
-    if show_tqdm:
-        iterator = tqdm(os.listdir(directory), **(tqdm_kwargs or {}))
-    else:
-        iterator = os.listdir(directory)
+    iterator = tqdm(audio_index.items(), **(tqdm_kwargs or {})) \
+        if show_tqdm else audio_index.items()
 
-    extensions = (".flac", ".mp3", ".ogg", ".wav")
-    for file_name in iterator:
-        if not file_name.endswith(extensions):
-            continue
-        file_path = os.path.join(directory, file_name)
-
-        embedding = embeddings.get(file_path)
-        if embedding is None:
+    for audio_path, audio_data in iterator:
+        audio_embedding, audio_hash = audio_data
+        if audio_embedding is None:
             try:
-                embedding = inference(file_path)
+                audio_embedding = inference(audio_path)
             except Exception as e:
-                message = f"Failed to compute embeddings for {file_path}: {e}"
-                tqdm.write(message)
+                error_message = f"Failed to compute embeddings for {audio_path}: {e}"
+                tqdm.write(error_message)
                 original_level = logger.level
                 logger.setLevel(logging.CRITICAL)
-                logger.error(message)
+                logger.error(error_message)
                 logger.setLevel(original_level)
                 continue
-        embeddings[file_path] = embedding
+        audio_index[audio_path] = (audio_embedding, audio_hash)
 
 
 def compute_distances(
-    enrollment_embeddings: Dict[str, np.ndarray],
-    test_embeddings: Dict[str, np.ndarray],
+    enrollment_index: Dict[str, Tuple[np.ndarray, bytes]],
+    test_index: Dict[str, Tuple[np.ndarray, bytes]],
     show_tqdm: bool = True,
     tqdm_kwargs: Dict[str, Any] = None
 ) -> Dict[str, float]:
@@ -72,10 +82,8 @@ def compute_distances(
     Compute average cosine distances between enrollment and test embeddings.
 
     Parameters:
-        enrollment_embeddings (Dict[str, np.ndarray]): Dictionary of enrollment embeddings.
-            Keys are file paths, values are enrollment embeddings.
-        test_embeddings (Dict[str, np.ndarray]): Dictionary of test embeddings.
-            Keys are file paths, values are test embeddings.
+        enrollment_index (Dict[str, Tuple[np.ndarray, bytes]]): Dictionary of enrollment embeddings and file hashes.
+        test_index (Dict[str, Tuple[np.ndarray, bytes]]): Dictionary of test embeddings and file hashes.
         show_tqdm (bool, optional): Whether to show a tqdm progress bar.
         tqdm_kwargs (Dict[str, Any], optional): Additional arguments for the tqdm progress bar.
 
@@ -83,26 +91,26 @@ def compute_distances(
         Dict[str, float]: Dictionary of distances. Keys are test file paths, values are the average cosine distances
             from the test embeddings to the enrollment embeddings. Lower distances indicate better matches.
     """
-    if show_tqdm:
-        iterator = tqdm(enrollment_embeddings.items(), **(tqdm_kwargs or {}))
-    else:
-        iterator = enrollment_embeddings.items()
+    iterator = tqdm(enrollment_index.items(), **(tqdm_kwargs or {})) \
+        if show_tqdm else enrollment_index.items()
 
     distances = {}
-    for enrollment_path, enrollment_embedding in iterator:
-        for test_path, test_embedding in test_embeddings.items():
+    for enrollment_path, enrollment_data in iterator:
+        enrollment_embedding, _ = enrollment_data
+        for test_path, test_data in test_index.items():
+            test_embedding, _ = test_data
             distance = cosine(enrollment_embedding.flatten(), test_embedding.flatten())
             if np.isnan(distance):
                 distance = float("inf")
             distances[test_path] = (distances.get(test_path) or 0) + distance
-    distances = {k: v / len(enrollment_embeddings) for k, v in distances.items()}
+    distances = {k: v / len(enrollment_index) for k, v in distances.items()}
     return distances
 
 
 def identify_speaker(
-    enrollment_path: str,
-    test_path: str,
-    save_path: Optional[str] = None,
+    enrollment_dir: str,
+    test_dir: str,
+    save_dir: Optional[str] = None,
     device: Optional[str] = None,
     hf_api_token: Optional[str] = None,
     show_tqdm: bool = True
@@ -111,9 +119,9 @@ def identify_speaker(
     Compute embeddings and distances for audio files for speaker identification.
 
     Parameters:
-        enrollment_path (str): Path to the directory containing the enrollment audio files.
-        test_path (str): Path to the directory containing the test audio files.
-        save_path (str, optional): Path to the directory where embeddings and distances will be saved.
+        enrollment_dir (str): Path to the directory containing the enrollment audio files.
+        test_dir (str): Path to the directory containing the test audio files.
+        save_dir (str, optional): Path to the directory where embeddings and distances will be saved.
         device (str, optional): Device to use for inference. Defaults to "cuda" if available, else "cpu".
         hf_api_token (str, optional): Hugging Face API token for model authentication.
         show_tqdm (bool, optional): Whether to show a tqdm progress bar.
@@ -128,10 +136,10 @@ def identify_speaker(
     device = torch.device(device or ("cuda" if torch.cuda.is_available() else "cpu"))
 
     # Create directories
-    os.makedirs(enrollment_path, exist_ok=True)
-    os.makedirs(test_path, exist_ok=True)
-    if save_path is not None:
-        os.makedirs(save_path, exist_ok=True)
+    os.makedirs(enrollment_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+    if save_dir is not None:
+        os.makedirs(save_dir, exist_ok=True)
 
     # Load model
     model = Model.from_pretrained(
@@ -145,27 +153,42 @@ def identify_speaker(
     )
     logger.info(f"Loaded model to {device}")
 
-    enrollment_embeddings = dict()
-    test_embeddings = dict()
+    # file_path: (embedding, file_hash)
+    enrollment_index: Dict[str, Tuple[Optional[np.ndarray], bytes]] = dict()
+    test_index: Dict[str, Tuple[Optional[np.ndarray], bytes]] = dict()
+
+    # Compute hashes
+    pairs = [
+        (enrollment_dir, enrollment_index),
+        (test_dir, test_index)
+    ]
+    extensions = (".flac", ".mp3", ".ogg", ".wav")
+    for audio_dir, audio_index in pairs:
+        for file_name in os.listdir(audio_dir):
+            if not file_name.endswith(extensions):
+                continue
+            audio_path = os.path.join(audio_dir, file_name)
+            audio_hash = hash_file(audio_path)
+            audio_index[audio_path] = (None, audio_hash)
 
     # Load embeddings if they exist
-    if save_path is not None:
-        enrollment_embeddings_path = os.path.join(save_path, "enrollment_embeddings.pkl")
-        test_embeddings_path = os.path.join(save_path, "test_embeddings.pkl")
+    cached_embeddings_path = os.path.join(save_dir, "embeddings_cache.pkl") if save_dir else None
+    cached_embeddings: Dict[bytes, np.ndarray] = dict()
+    if save_dir and os.path.exists(cached_embeddings_path):
+        with open(cached_embeddings_path, "rb") as f:
+            cached_embeddings = pickle.load(f)
+        logger.info(f"Loaded {len(cached_embeddings)} cached embeddings from {cached_embeddings_path}")
 
-        if os.path.exists(enrollment_embeddings_path):
-            with open(enrollment_embeddings_path, "rb") as f:
-                enrollment_embeddings = pickle.load(f)
-            logger.info(f"Loaded {len(enrollment_embeddings)} enrollment embeddings from {enrollment_embeddings_path}")
-        if os.path.exists(test_embeddings_path):
-            with open(test_embeddings_path, "rb") as f:
-                test_embeddings = pickle.load(f)
-            logger.info(f"Loaded {len(test_embeddings)} test embeddings from {test_embeddings_path}")
+        for audio_dir, audio_index in pairs:
+            for audio_path, audio_data in audio_index.items():
+                _, audio_hash = audio_data
+                audio_embedding = cached_embeddings.get(audio_hash)
+                if audio_embedding is not None:
+                    audio_index[audio_path] = (audio_embedding, audio_hash)
 
     # Compute enrollment embeddings
     compute_embeddings(
-        enrollment_path,
-        enrollment_embeddings,
+        enrollment_index,
         inference,
         show_tqdm=show_tqdm,
         tqdm_kwargs={
@@ -174,15 +197,17 @@ def identify_speaker(
     )
 
     # Serialize enrollment embeddings
-    if save_path is not None:
-        with open(enrollment_embeddings_path, "wb") as f:
-            pickle.dump(enrollment_embeddings, f)
-        logger.info(f"Enrollment embeddings saved to {enrollment_embeddings_path}")
+    if save_dir is not None:
+        cached_embeddings.update({
+            audio_hash: audio_embedding for _, (audio_embedding, audio_hash) in enrollment_index.items()
+        })
+        with open(cached_embeddings_path, "wb") as f:
+            pickle.dump(cached_embeddings, f)
+        logger.info(f"Cached enrollment embeddings saved to {cached_embeddings_path}")
 
     # Compute test embeddings
     compute_embeddings(
-        test_path,
-        test_embeddings,
+        test_index,
         inference,
         show_tqdm=show_tqdm,
         tqdm_kwargs={
@@ -191,15 +216,18 @@ def identify_speaker(
     )
 
     # Serialize test embeddings
-    if save_path is not None:
-        with open(test_embeddings_path, "wb") as f:
-            pickle.dump(test_embeddings, f)
-        logger.info(f"Test embeddings saved to {test_embeddings_path}")
+    if save_dir is not None:
+        cached_embeddings.update({
+            audio_hash: audio_embedding for _, (audio_embedding, audio_hash) in test_index.items()
+        })
+        with open(cached_embeddings_path, "wb") as f:
+            pickle.dump(cached_embeddings, f)
+        logger.info(f"Cached test embeddings saved to {cached_embeddings_path}")
 
     # Compute distances
     distances = compute_distances(
-        enrollment_embeddings,
-        test_embeddings,
+        enrollment_index,
+        test_index,
         show_tqdm=show_tqdm,
         tqdm_kwargs={
             "desc": "Computing distances"
@@ -208,11 +236,11 @@ def identify_speaker(
     sorted_distances = sorted(distances.items(), key=lambda x: x[1])
 
     # Serialize distances
-    if save_path is not None:
-        for i, (path, distance) in enumerate(sorted_distances):
+    if save_dir is not None:
+        for i, (audio_dir, distance) in enumerate(sorted_distances):
             if distance == float("inf"):
-                sorted_distances[i] = (path, None)
-        distances_path = os.path.join(save_path, "distances.json")
+                sorted_distances[i] = (audio_dir, None)
+        distances_path = os.path.join(save_dir, "distances.json")
         with open(distances_path, "w") as f:
             json.dump(sorted_distances, f, indent=4)
         logger.info(f"Distances saved to {distances_path}")
@@ -258,9 +286,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     identify_speaker(
-        enrollment_path=args.enrollment_path,
-        test_path=args.test_path,
-        save_path=args.save_path,
+        enrollment_dir=args.enrollment_path,
+        test_dir=args.test_path,
+        save_dir=args.save_path,
         device=args.device,
         hf_api_token=args.hf_api_token
     )
